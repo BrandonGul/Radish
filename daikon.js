7350b9b82c039aab4a6f82f5cd873c53873d8aa8
BrandonGul <bsguly@gmail.com>
2020-04-09 13:57:46 -0500

    Added daikon.js
    Added .gitignore

---
+++ daikon.js
@@ -0,0 +1,27 @@
+const fs = require('fs')
+
+var dict = {};
+
+exports.loadDirctory = function(filePath) {
+  fs.readdir(filePath, function(err, files) {
+    files
+    .filter(function(file) {
+      return file.substr(-5) === '.html';
+    });
+    .forEach(function(file) {
+      fs.readFile(filePath + '/' + file, function(err, data) { dict[file] = data.toString()});
+    });
+  })
+}
+
+exports.format = function (fileName, array, callback) {
+  if (dict.hasOwnProperty(fileName)) {
+    var str = dict[fileName];
+    for (var i = 0; i < array.length; i++) {
+      for (var key in array[i]) {
+        str = str.replace(key, array[i][key]);
+      }
+    }
+    return str;
+  }
+}
